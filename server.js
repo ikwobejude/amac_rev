299dd5146c39cc0ba68e1f1659c93fc09d1b2efb
ikwobejude <ikwobejude@gmail.com>
2023-04-16 23:25:40 +0100

    memory leak fixed

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = require("./app");
 
 
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () => {
   console.log(`App running at port ${PORT}`);
